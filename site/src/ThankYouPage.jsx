c02f37ad7c798618a7c994e062473a8f2a679274
phuongdoan219 <doanvanphuong219@gmail.com>
2026-09-14 15:38:57 +0700

    update group zalo

--- site/src/ThankYouPage.jsx
+++ site/src/ThankYouPage.jsx
@@ -36,7 +36,7 @@ export function ThankYouPage() {
         </div>
 
         <a
-          href="https://zalo.me/g/4bl7yc9t8d3hfym4bvpc"
+          href="https://zalo.me/g/71inod1r7w2wrblvtiy5"
           target="_blank"
           rel="noreferrer"
         >
